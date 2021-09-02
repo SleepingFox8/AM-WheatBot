36c455e868b63897e41a0c02a4f22dd1d79f8e9a
SleepingFox8 <SleepingFox8@gmail.com>
2021-09-02 15:15:42 -0400

    fix dropSeeds.lua

--- dropSeeds.lua
+++ dropSeeds.lua
@@ -1,2 +1,12 @@
-local botTools = import("AM-BotTools/botTools")
-botTools.dropAllOfItemExcept("minecraft:wheat_seeds",3)
+-- initialization
+    -- ensure imports are from file instead of cache
+        local function import(path)
+            package.loaded[path] = nil
+            local imported = require (path)
+            package.loaded[path] = nil
+            return imported
+        end
+    -- import dependancies
+        local botTools = import("AM-BotTools/botTools")
+-- main program
+    botTools.dropAllOfItemExcept("minecraft:wheat_seeds",3)
